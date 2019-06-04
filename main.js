28b526780b879b85068a39afaef1b897cd647338
elliaster <30734189+elliaster@users.noreply.github.com>
2019-06-04 13:44:49 -0400

    changed nav bar again, changes to layout of navbar and styling

--- main.js
+++ main.js
@@ -21,6 +21,35 @@ menu.click(function () {
 	}
 });
 
-resume.click(function () {
-	location.href = "";
+const links = {
+	resume: "https://www.google.com",
+	linkedin: "https://www.linkedin.com/in/mohammed-h-8043b913a/",
+	github: "https://github.com/elliaster",
+	twitter: "https://twitter.com/Elliaster_"
+};
+
+$(".links").click(function () {
+	var index = $(".links").index(this);
+	if (index == 0) {
+		location.href = links.github
+	} else if (index == 1) {
+		location.href = links.linkedin
+	} else if (index == 2) {
+		location.href = links.twitter
+	} else {
+		location.href = links.resume
+	}
+});
+
+$(".links").hover(function () {
+	var index = $(".links").index(this), text = $("#sample-text");
+	if (index == 0) {
+		text.text("GitHub")
+	} else if (index == 1) {
+		text.text("LinkedIn")
+	} else if (index == 2) {
+		text.text("Twitter")
+	} else {
+		text.text("Resume Download")
+	}
 });
